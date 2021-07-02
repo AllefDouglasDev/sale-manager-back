ba45b67e16f5158ab449f8547ccbce826090776f
Allef Douglas <allefwalker73@gmail.com>
2021-07-02 14:49:38 -0300

    jest configuration

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  moduleFileExtensions: ["js", "json", "jsx", "ts", "tsx", "node"],
+  testEnvironment: "node",
+  preset: "ts-jest",
+};
